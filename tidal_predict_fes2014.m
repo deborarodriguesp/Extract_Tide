fb7c1d131b4cb3c33d68f8f04d279c934a7f8022
Débora Rodrigues Pereira <123467022+deborarodriguesp@users.noreply.github.com>
2024-03-18 21:50:09 +0000

    Add files via upload
    script for matlab to predict tide from FES2014 using a few functions from the t_tide package.

---
+++ tidal_predict_fes2014.m
@@ -0,0 +1,77 @@
+clc
+clear all
+close all
+
+%% Specify the folder where your files are located
+folder = 'D:/DOUTORAMENTO/Tide/Tide_Extractor/output_extract/';
+output_folder = 'D:/DOUTORAMENTO/Tide/Tide_Extractor/output_predict/';
+% Get a list of all files in the folder
+fileList = dir(fullfile(folder, 'tidal_data_point_*.txt'));
+
+%% Loop through each file
+for i = 1 : numel(fileList)
+    % Build the full path to the current file
+    filePath = fullfile(folder, fileList(i).name);
+    
+    % Extract latitude and longitude from the file name
+    LatLong = sscanf(fileList(i).name, 'tidal_data_point_%f_%f.txt');
+    latitude = LatLong(1);
+    longitude = LatLong(2);
+    
+    % Corrected syntax for t_predic
+    start_date = datenum(2012, 11, 01, 00, 00, 00); 
+    end_date = datenum(2014, 01, 01, 00, 00, 00);   % 10 de janeiro de 2008
+    dt=datenum(0000,00,00,01,00,00);
+    t =(start_date:dt:end_date);
+    date = datetime(t,'ConvertFrom','datenum');
+    %%
+    % Read the data from the file
+    data = readtable(filePath);
+    
+    % Extract necessary columns (Harmonics, Amplitude, Phase)
+    name = data.Harmonics;
+    amplitude = data.Amplitude;
+    phase = data.Phase; 
+    freq = t_tide_name2freq(name,'unit','rad/day')';
+    
+    % Selecione apenas as componentes válidas para calcular as previsões das marés
+    const=t_getconsts;
+    valid_indices = ismember(name, const.name);
+    valid_name = name(valid_indices, :);
+    valid_amplitude = amplitude(valid_indices);
+    valid_phase = phase(valid_indices);
+    
+    tidecon= [freq valid_amplitude valid_phase];
+    
+    %%
+    % Inicialize a matriz para armazenar os sinais de maré para cada momento
+    all_signals = zeros(length(t), size(tidecon, 1));
+
+    % Calcule os sinais harmônicos para cada momento individualmente
+    for i = 1:length(t)
+        for k = 1:size(tidecon, 1)
+            % Calcule o sinal harmônico para o momento atual
+            signal = tidecon(k, 2) * sin((tidecon(k, 1) * t(i)) + tidecon(k, 3));
+
+            % Armazene o sinal harmônico na matriz
+            all_signals(i, k) = signal;
+        end
+    end
+
+    % Some todos os sinais harmônicos para obter o sinal total de maré para cada momento
+    tide = sum(all_signals, 2);
+
+%%  
+    interval=1; 
+    
+    [NAME,FREQ,TIDECON]=t_tide(tide(:,1),interval,'start time',[2012, 11, 01, 00, 00, 00],'latitude',latitude,'synthesis',2);
+    YOUT = t_predic(t,NAME,FREQ,TIDECON,'latitude',latitude,'synthesis',2);
+   
+%%
+    
+    filename = sprintf('tidal_predict_%.6f_%.6f.txt', latitude, longitude);
+    filepath = fullfile(output_folder, filename);
+    dlmwrite(filepath, tide, 'precision', 4); % Salva o arquivo com precisão de 10 dígitos
+    
+    
+end
